c2734a0baee758b8841a2be54af29433c2fde326
Ubaid Ullah Mirza <167180333+UbaidMirza1234@users.noreply.github.com>
2025-03-26 21:42:57 +0530

    Add files via upload

---
+++ Qr code.py
@@ -0,0 +1,9 @@
+import qrcode
+
+data =input('enter the text or url:').strip()
+filename = input('enter the fiename:').strip()
+qr = qrcode.QRCode(box_size=10,border=4)
+qr.add_data(data)
+image= qr.make_image(fill_color='black',back_color='white')
+image.save(filename)
+print(f'QR Code saved as {filename}')
